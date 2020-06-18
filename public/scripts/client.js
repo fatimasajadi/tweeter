3983a77153f0e85366033f443954faba82c9415a
fsajadi <s.sfatemeh@gmail.com>
2020-06-18 10:46:27 -0400

    Implement the client side logic of the app, post new tweets to /tweets and get old tweets from server to populate the template html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,74 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  $(".error").hide();
+
+  const createTweetElement = function(tweet) {
+    const $tweet = $(`
+    <div class="tweet">
+      <header class="tweet-header">
+          <img src="${tweet.user.avatars}">
+          <p>${tweet.user.handle}</p>
+      </header>
+      <article>
+            <p>${tweet.content.text.replace('<', '&lt;').replace('>', '&gt;')}</p>
+      </article>
+      <footer>
+          <p>${tweet.created_at}</p>
+          <span>
+            <img src="/images/flag24.png">
+            <img src="/images/retweet30px.png">
+            <img src="/images/heart.png">
+          </span>
+      </footer>
+    </div>
+  `);
+    return $tweet;
+  }
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      $('.tweet-container').prepend(createTweetElement(tweet));
+    }
+  }
+
+  $(".tweet-form").on('submit', function(event) {
+    event.preventDefault();
+
+    let text = $(".tweet-textarea").val();
+    if (text.length <= 140 && text.length !== 0) {
+      $.post('/tweets', { text: text })
+        .then(function(data) {});
+      loadTweets();
+
+    } else {
+      // alert("Your tweet is too long to be published!")
+      $(".error").append("Too long. Plz respect our arbitrary limit of 140 chars.");
+      $(".error").show();
+    }
+
+  });
+
+  function loadTweets() {
+    url = "/tweets/";
+    $.ajax({
+        url,
+        method: 'GET',
+      })
+      .done(function(response) {
+        console.log(response);
+        $('.tweet-container').html('');
+        renderTweets(response);
+      })
+      .fail(function() {
+        console.log('error');
+      })
+      .always(function() {
+        console.log('complete');
+      });
+
+  };
+
+  loadTweets();
+});
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+  $(".tweet-textarea").on("input", function(event) {
+    let textInput = $(this).val();
+    let textInputLen = textInput.length;
+
+    let counter = 140 - textInputLen;
+    let parentText = $(event.target).closest('form');
+    let counterUpdate = parentText.find('.counter');
+    if (counter >= 0) {
+      counterUpdate.html(counter);
+    } else {
+      counterUpdate.html(counter);
+      (counterUpdate).css({ 'color': 'red' });
+    }
+  })
+});
